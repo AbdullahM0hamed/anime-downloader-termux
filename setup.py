e28dc3b87f4f24964e045aa2965e4d8eb559d43a
AbdullahM0hamed <25087116+AbdullahM0hamed@users.noreply.github.com>
2020-06-26 01:12:25 +0100

    Add ability to share anime URLs to termux

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 from setuptools import setup
+from shutil import copyfile
+import os
 
 setup(
     name='anime-downloader-termux',
@@ -10,3 +12,18 @@ setup(
         'anime-downloader @ git+https://github.com/vn-ki/anime-downloader'
     ]
 )
+
+bin_dir = "/data/data/com.termux/files/home/bin"
+
+try:
+    os.makedirs(bin_dir)
+except FileExistsError:
+    if os.path.exists(f"{bin_dir}/termux-url-opener"):
+        #For some reason, if user doesn't have termux-url-opener, it creates the one I want then runs again and copies that to termux-url-opener-2
+        new = open("termux-url-opener").read()
+        current = open(f"{bin_dir}").read()
+
+        if current != new:
+            os.rename(f"{bin_dir}/termux-url-opener", f"{bin_dir}/termux-url-opener-2")
+
+copyfile("termux-url-opener", f"{bin_dir}/termux-url-opener")
